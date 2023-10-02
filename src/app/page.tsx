8959069324aebf497f7043c9d49d5785f2ba85e3
baizhi958216 <1475289190@qq.com>
2023-10-02 17:28:59 +0800

    feat: 生成一条todo数据并显示
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,37 @@
-export default function Home() {
-  return <h1>Hi</h1>
+import { prisma } from "@/db";
+import Link from "next/link";
+
+function getTodos() {
+  return prisma.todo.findMany();
+}
+
+export default async function Home() {
+  // 新增一条数据
+  /* await prisma.todo.create({
+    data: {
+      title: "Todo 1",
+      complete: false,
+    },
+  }); */
+  const todos = await getTodos();
+  return (
+    <>
+      <header className="flex justify-between items-center mb-4">
+        <h1 className="text-2xl">Todos</h1>
+        <Link
+          className="border border-slate-300 text-slate-300 px-2 py-1 rounded
+        hover:bg-slate-700 focus-within:bg-slate-700 outline-none
+        "
+          href="/new"
+        >
+          New
+        </Link>
+      </header>
+      <div className="pl-4">
+        {todos.map((todo) => {
+          return <ul key={todo.id}>{todo.title}</ul>;
+        })}
+      </div>
+    </>
+  );
 }
